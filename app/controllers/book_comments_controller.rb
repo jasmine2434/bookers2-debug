bf8a67d4b204b505d47937cf6b1c236a6ca7ea66
EC2 Default User <ec2-user@ip-172-31-0-96.ap-northeast-1.compute.internal>
2024-08-31 23:13:20 +0000

    search

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,19 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    
   end
   
   
   def destroy
-    comment = BookComment.find(params[:id])
-    comment.destroy
-    redirect_to book_book_comment_path(params[:post_book_id])
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    
   end
   
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
       resource :favorite, only: [:create, :destroy]
